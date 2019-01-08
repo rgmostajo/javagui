bc7a31244faec54e0812a8d48e27700118c04292
rgmostajo <ricamostajo@gmail.com>
2019-01-08 12:48:33 +0800

    add frame

--- BasicElements.java
+++ BasicElements.java
@@ -1,6 +1,14 @@
+import javax.swing.JFrame;
+
 public class BasicElements{
 
 	public static void main(String args[]){
+		JFrame myFrame = nre JFrame();
+
+		myFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+		myFrame.setSize(900, 600);
+
 		System.out.println("basic java elements");
 	}
 }
